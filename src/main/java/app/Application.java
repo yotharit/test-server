21ed34b8392ec680adfbcba6c36d1d602821d4e0
Tharit Pongsaneh <tharitpongsaneh@Tharits-MacBook-Pro-2.local>
2019-03-03 22:24:58 +0700

    Add ip show

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,11 +1,20 @@
 package app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+
+    @Value("${local.server.port}")
+
+    public static void main(String[] args) throws UnknownHostException {
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        System.out.println(ip);
         SpringApplication.run(Application.class, args);
     }
 }
